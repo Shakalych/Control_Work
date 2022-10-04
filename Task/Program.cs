1cd9d32f0d44216ccb7b240504e0a86b68ed3401
Shakalych <madstabr@gmail.com>
2022-10-04 17:36:54 +0500

    Программа контрольной работы

---
+++ Task/Program.cs
@@ -0,0 +1,31 @@
+﻿//Контрольная работа
+
+string [] array1 = {"Brain+1", "Space", "Geek", "Hellow", "World", "2", "123",  "Go", "Fun"};
+string [] array2 = new string[array1.Length];
+PrintArray(array1);
+ChangeArray(array1, array2);
+PrintArray(array2);
+
+
+void PrintArray(string [] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+void ChangeArray(string [] array1, string [] array2)
+{
+
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+            {
+            array2[count] = array1[i];
+            count++;
+            }
+    }
+}
